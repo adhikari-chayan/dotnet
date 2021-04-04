ba839c3170f93c1ab1fe42dfcd5c75d0b9b4aec1
Chayan Adhikari <chayan.adhikari@ivycomptech.com>
2021-04-04 13:05:32 +0530

    some new projects from my learning in advanced collections

---
+++ AdvancedCollections/BankHolidays/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankHolidays
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			List<DateTime> bankHols1 = new List<DateTime>
+			{
+				new DateTime(2021, 1, 1),
+				new DateTime(2021, 4, 2),
+				new DateTime(2021, 4, 5),
+				new DateTime(2021, 5, 3),
+				new DateTime(2021, 5, 31),
+				new DateTime(2021, 8, 30),
+				new DateTime(2021, 12, 27),
+				new DateTime(2021, 12, 28),
+			};
+
+			bankHols1.Add(new DateTime(2021, 4, 1));
+
+			DateTime[] bankHols2 =
+			{
+				new DateTime(2021, 1, 1),
+				new DateTime(2021, 4, 2),
+				new DateTime(2021, 4, 5),
+				new DateTime(2021, 5, 3),
+				new DateTime(2021, 5, 31),
+				new DateTime(2021, 8, 30),
+				new DateTime(2021, 12, 27),
+				new DateTime(2021, 12, 28),
+			};
+
+
+			//DateTime[] bankHols3 = bankHols1;
+ 
+            //Console.WriteLine($"bh1==bh2? {bankHols1==bankHols2}");
+			//Console.WriteLine($"bh2==bh3? {bankHols2 == bankHols3}");
+			//Console.WriteLine($"bh3==bh1? {bankHols3 == bankHols1}");
+
+
+			Console.WriteLine($"equal values? {bankHols1.SequenceEqual(bankHols2)}");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ AdvancedCollections/TourBooker/TourBooker.Logic/AppData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TourBooker.Logic;
+
+namespace Pluralsight.AdvCShColls.TourBooker.Logic
+{
+	public class AppData
+	{
+		public List<Country> AllCountries { get; private set; }
+		//public SortedList<string, Country> AllCountriesByKey { get; private set; }
+		public Dictionary<CountryCode,Country> AllCountriesByKey { get; private set; }
+
+		public void Initialize(string csvFilePath)
+		{
+			CsvReader reader = new CsvReader(csvFilePath);
+			//this.AllCountries = reader.ReadAllCountries();
+
+			// during the module, the above line is changed to
+			this.AllCountries = reader.ReadAllCountries().OrderBy(x=>x.Name).ToList();
+
+			//var dict = this.AllCountries.ToDictionary(x => x.Code, StringComparer.OrdinalIgnoreCase);
+			//this.AllCountriesByKey = new SortedList<string, Country>(dict);
+
+			this.AllCountriesByKey = this.AllCountries.ToDictionary(x => x.Code);
+
+
+		}
+	}
+}
