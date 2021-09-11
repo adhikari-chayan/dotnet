c0105b5e3ac6d8febc239e8e04ba2479e823af90
Chayan Adhikari <chayan.adhikari@ivycomptech.com>
2021-09-12 04:22:58 +0530

    some more concurrent collection code

---
+++ ConcurrentCollections/5.Performance/DictionaryPerformance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+
+namespace Pluralsight.ConcurrentCollections.DictionaryPerformance
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int dictSize = 200000;
+
+			Console.WriteLine("Dictionary, single thread:");
+			var dict = new Dictionary<int, int>();
+			SingleThreadBenchmark.Benchmark(dict, dictSize);
+
+			Console.WriteLine("\r\nConcurrentDictionary, single thread:");
+			var dict2 = new ConcurrentDictionary<int, int>();
+			SingleThreadBenchmark.Benchmark(dict2, dictSize);
+
+			Console.WriteLine("\r\nConcurrentDictionary, multiple threads:");
+			var dict3 = new ConcurrentDictionary<int, int>();
+			ParallelBenchmark.Benchmark(dict3, dictSize);
+		}
+	}
+}
